b72a2b3ef2c9c957438d8de87ab861620ca2c450
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-13 18:25:47 -0300

    feat(config): configura o diretório dos arquivos estáticos do projeto e o diretório alvo do upload de arquivos

--- seletive/settings.py
+++ seletive/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+# Upload files
+# Configuração do diretório alvo do upload de arquivos
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
