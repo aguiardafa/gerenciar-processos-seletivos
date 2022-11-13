d57912da8b209664f32ebaf7bba3b917f5ec9051
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-13 20:37:20 -0300

    feat(gerenc_empresa): registra as models no admin.py

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Tecnologias, Empresa, Vagas
+
 # Register your models here.
+admin.site.register(Tecnologias)
+admin.site.register(Empresa)
+admin.site.register(Vagas)
