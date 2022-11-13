9e499f06bc515813e654696cde168f8c0902d304
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-13 20:43:46 -0300

    feat(gerenc_empresa): preenche o campo tecnologias dinamicamente

--- empresa/views.py
+++ empresa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tecnologias
 
 
 def nova_empresa(request):
-    return render(request, 'nova_empresa.html')
+    techs = Tecnologias.objects.all()
+    return render(request, 'nova_empresa.html', {'techs': techs})
