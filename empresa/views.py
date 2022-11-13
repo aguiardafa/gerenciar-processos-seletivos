0eb48bd0cd09c4637978e84bac0885d8e6aa77d7
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-11-13 18:08:55 -0300

    feat(gerenc_empresa): cria URL para o app e a primeira rota para view de gerenciamento de empresas

--- empresa/views.py
+++ empresa/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def nova_empresa(request):
+    return HttpResponse('Página de nova_empresa')
